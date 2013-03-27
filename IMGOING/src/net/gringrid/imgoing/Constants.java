fd51308a872bccb845927c452a1c1d77d6d102b9
choijiho <nisclan@hotmail.com>
2013-03-28 08:57:36 +0900

    test

--- IMGOING/src/net/gringrid/imgoing/Constants.java
+++ IMGOING/src/net/gringrid/imgoing/Constants.java
@@ -1,6 +1,7 @@
 package net.gringrid.imgoing;
 
 public final class Constants {
+	
 	public static final String BROADCAST_ACTION = "TEST_BROADCAST_ACTION";
 	public static final String LOCATION_SERVICE = "net.gringrid.imgoing.location.SendCurrentLocationService";
 
